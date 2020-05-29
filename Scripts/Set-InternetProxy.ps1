e9b6bf8cf9332d16d2c16c46f7d231866a96ae4c
greenSacrifice <emails@joshuavandaalen.com.au>
2020-05-29 19:34:06 +1000

    Upload Squid Proxy registry changes

---
+++ Scripts/Set-InternetProxy.ps1
@@ -0,0 +1,19 @@
+﻿
+$RootDir = "C:/"
+If (Test-Path $RootDir -eq $false) { mkdir $RootDir }
+
+$FileLocation = "$RootDir/ProxyAutoConfiguration.js"
+$ProxyServer = '192.168.0.200' # IP of Squid server
+$Port = '3128' # Default Squid server port number
+
+"function FindProxyForURL(url, host) {
+    if (shExpMatch(host, '(*joshuavandaalen.com.au*)'))
+        return 'PROXY $ProxyServer`:$Port';
+    return 'DIRECT';    
+}" > $FileLocation
+
+$RegKey = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings"
+
+Set-ItemProperty -path $RegKey ProxyEnable -value 1
+Set-ItemProperty -path $RegKey ProxyServer -value "$ProxyServer`:$Port"
+Set-ItemProperty -path $RegKey AutoConfigURL -Value "file:///$FileLocation"
