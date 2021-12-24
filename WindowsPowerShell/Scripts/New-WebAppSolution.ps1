c48ff3f4b5b0b65ecc3a5cdf68449a7ddc619990
Joshua Van Daalen <emails@joshuavandaalen.com.au>
2021-12-24 19:06:33 +1100

    Update to create apps for core 3.1

--- WindowsPowerShell/Scripts/New-WebAppSolution.ps1
+++ WindowsPowerShell/Scripts/New-WebAppSolution.ps1
@@ -1,14 +1,23 @@
+$AppName = 'Company'
 
-$AppName = 'MyAccount-Company'
+mkdir "/git/$AppName/src"
 
-dotnet new sln -n $AppName
-dotnet new mvc -o ".\src\$AppName-App" -n "$AppName-App"
-dotnet new classlib -o ".\src\$AppName-Common" -n "$AppName-Common"
-dotnet new classlib -o ".\src\$AppName-Models" -n "$AppName-Models"
-dotnet new classlib -o ".\src\$AppName-Repo" -n "$AppName-Repo"
-dotnet new classlib -o ".\src\$AppName-Svc" -n "$AppName-Svc"
-dotnet sln "$AppName.sln" add ".\src\$AppName-App\$AppName-App.csproj" --in-root
-dotnet sln "$AppName.sln" add ".\src\$AppName-Common\$AppName-Common.csproj" --in-root
-dotnet sln "$AppName.sln" add ".\src\$AppName-Models\$AppName-Models.csproj" --in-root
-dotnet sln "$AppName.sln" add ".\src\$AppName-Repo\$AppName-Repo.csproj" --in-root
-dotnet sln "$AppName.sln" add ".\src\$AppName-Svc\$AppName-Svc.csproj" --in-root
+cd "/git/$AppName"
+
+dotnet new sln -o "./src/" -n $AppName
+dotnet new mvc -o "./src/$AppName.App" -n "$AppName.App" --framework netcoreapp3.1
+dotnet new classlib -o "./src/$AppName.Common" -n "$AppName.Common" --framework netcoreapp3.1
+dotnet new classlib -o "./src/$AppName.Models" -n "$AppName.Models" --framework netcoreapp3.1
+dotnet new classlib -o "./src/$AppName.Repo" -n "$AppName.Repo" --framework netcoreapp3.1
+dotnet new classlib -o "./src/$AppName.Svc" -n "$AppName.Svc" --framework netcoreapp3.1
+dotnet sln "./src/$AppName.sln" add "./src/$AppName.App/$AppName.App.csproj" --in-root
+dotnet sln "./src/$AppName.sln" add "./src/$AppName.Common/$AppName.Common.csproj" --in-root
+dotnet sln "./src/$AppName.sln" add "./src/$AppName.Models/$AppName.Models.csproj" --in-root
+dotnet sln "./src/$AppName.sln" add "./src/$AppName.Repo/$AppName.Repo.csproj" --in-root
+dotnet sln "./src/$AppName.sln" add "./src/$AppName.Svc/$AppName.Svc.csproj" --in-root
+
+foreach ($f in (ls "/git/$AppName/src" -File -Recurse)) {
+    if ($f.name -eq 'Class1.cs') {
+        rm $($f.fullname)
+    }
+}
